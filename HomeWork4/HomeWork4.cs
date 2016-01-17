b81aca566156fb19e08491b1a99ccac4be583e1b
KOKO666 <koko_666@mail.ru>
2016-01-17 15:55:50 +0200

    test1

---
+++ HomeWork4/HomeWork4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+using System.Text;
+
+
+class HomeWork4
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Console.OutputEncoding=Encoding.UTF8;
+        char c='\u20ac';
+        Console.WriteLine(c);
+
+        Console.WriteLine(DateTime.Now);
+
+        Console.WriteLine("dfgfdgdfgfd" + Environment.NewLine + "New Line");
+        Console.WriteLine("dfgfdgdfgfd" + "\nNew Line 6");
+
+
+
+        //int n;
+        //bool tryParceN = false;
+        //tryParceN = int.TryParse(Console.ReadLine(), out n);
+        //Console.WriteLine(n);
+        //Console.WriteLine(tryParceN);
+
+        double n;
+
+        try
+        {
+            n = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f2}",Math.Pow(n,.5));
+        }
+          catch (FormatException e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ home2/Home2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Home2
+{
+    static void Main()
+    {
+
+        int width = Convert.ToInt32(Console.ReadLine());
+        if (width >= 2 || width <= 75)
+        {
+            int height = 2 * width - 1;
+            int br = height;
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= width; j++)
+                {
+                    if (j == i || j == br)
+                        Console.Write("*");
+
+                    else if (j <= width)
+                        Console.Write(".");
+                }
+                br--;
+                Console.WriteLine();
+            }
+        }
+    }
+}
